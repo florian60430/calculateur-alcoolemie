88fef83ae3d5f99262899ad39414343c8c70b538
florian60430 <fwantelez@la-providence.net>
2020-01-22 16:46:27 +0100

    For (i=0; i<n i++) { heure et minute}

--- index.php
+++ index.php
@@ -46,51 +46,22 @@
 								<div class="col-1 marginlft">
 									<SELECT name="heurenow" size="1">
 										<OPTION value="?"> h</OPTION>
-										<OPTION value="00"> 00h</OPTION>
-										<OPTION value="01"> 01h</OPTION>
-										<OPTION value="02"> 02h</OPTION>
-										<OPTION value="03"> 03h</OPTION>
-										<OPTION value="04"> 04h</OPTION>
-										<OPTION value="05"> 05h</OPTION>
-										<OPTION value="06"> 06h</OPTION>
-										<OPTION value="07"> 07h</OPTION>
-										<OPTION value="08"> 08h</OPTION>
-										<OPTION value="09"> 09h</OPTION>
-										<OPTION value="10"> 10h</OPTION>
-										<OPTION value="11"> 11h</OPTION>
-										<OPTION value="12"> 12h</OPTION>
-										<OPTION value="13"> 13h</OPTION>
-										<OPTION value="14"> 14h</OPTION>
-										<OPTION value="15"> 15h</OPTION>
-										<OPTION value="16"> 16h</OPTION>
-										<OPTION value="17"> 17h</OPTION>
-										<OPTION value="18"> 18h</OPTION>
-										<OPTION value="19"> 19h</OPTION>
-										<OPTION value="20"> 20h</OPTION>
-										<OPTION value="21"> 21h</OPTION>
-										<OPTION value="22"> 22h</OPTION>
-										<OPTION value="23"> 23h</OPTION>
+
+										<?php for ($i = 0; $i < 24; $i++) {
+											echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
+										} ?>
+
 									</SELECT>
 								</div>
 								<!-- Select de des minutes acutelle -->
 								<div class="col-1 marginlft">
 									<SELECT name="minutenow" size="1">
 										<OPTION value="">min</OPTION>
-										<OPTION value="00"> 00</OPTION>
-										<OPTION value="05"> 05</OPTION>
-										<OPTION value="10"> 10</OPTION>
-										<OPTION value="15"> 15</OPTION>
-										<OPTION value="20"> 20</OPTION>
-										<OPTION value="25"> 25</OPTION>
-										<OPTION value="30"> 30</OPTION>
-										<OPTION value="35"> 35</OPTION>
-										<OPTION value="40"> 40</OPTION>
-										<OPTION value="45"> 45</OPTION>
-										<OPTION value="50"> 50</OPTION>
-										<OPTION value="55"> 55</OPTION>
+										<?php for ($i = 0; $i <= 59; $i++) {
+											echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
+										} ?>
 									</SELECT>
 								</div>
-
 							</div>
 						</div>
 						<div class="col-12 padding">
@@ -116,9 +87,9 @@
 								<div class="offset-1"></div>
 								<div class="col-2">
 									<!-- formulaire du poids de l'utilisateur -->
-									<input type="text" name="poid" value='' size="3" />
+									<input required type="text" name="poid" value='' size="3" />
 								</div>
-								<div class="col-1 marginltf text">KG</div>
+								<div class="col-1 marginltf2 text">KG</div>
 							</div>
 						</div>
 						<div class="col-12 padding">
@@ -149,48 +120,22 @@
 									<!-- Select de l'heure du premier verre -->
 									<SELECT name="premierh">
 										<OPTION value="?"> h</OPTION>
-										<OPTION value="00"> 00h</OPTION>
-										<OPTION value="01"> 01h</OPTION>
-										<OPTION value="02"> 02h</OPTION>
-										<OPTION value="03"> 03h</OPTION>
-										<OPTION value="04"> 04h</OPTION>
-										<OPTION value="05"> 05h</OPTION>
-										<OPTION value="06"> 06h</OPTION>
-										<OPTION value="07"> 07h</OPTION>
-										<OPTION value="08"> 08h</OPTION>
-										<OPTION value="09"> 09h</OPTION>
-										<OPTION value="10"> 10h</OPTION>
-										<OPTION value="11"> 11h</OPTION>
-										<OPTION value="12"> 12h</OPTION>
-										<OPTION value="13"> 13h</OPTION>
-										<OPTION value="14"> 14h</OPTION>
-										<OPTION value="15"> 15h</OPTION>
-										<OPTION value="16"> 16h</OPTION>
-										<OPTION value="17"> 17h</OPTION>
-										<OPTION value="18"> 18h</OPTION>
-										<OPTION value="19"> 19h</OPTION>
-										<OPTION value="20"> 20h</OPTION>
-										<OPTION value="21"> 21h</OPTION>
-										<OPTION value="22"> 22h</OPTION>
-										<OPTION value="23"> 23h</OPTION>
+
+										<?php for ($i = 0; $i < 24; $i++) {
+											echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
+										} ?>
+
 									</SELECT>
 								</div>
 								<!-- Select des minutes du premier verre -->
 								<div class="col-1 padding marginlft">
 									<SELECT name="premiermin">
-										<OPTION value="">min</OPTION>
-										<OPTION value="00"> 00</OPTION>
-										<OPTION value="05"> 05</OPTION>
-										<OPTION value="10"> 10</OPTION>
-										<OPTION value="15"> 15</OPTION>
-										<OPTION value="20"> 20</OPTION>
-										<OPTION value="25"> 25</OPTION>
-										<OPTION value="30"> 30</OPTION>
-										<OPTION value="35"> 35</OPTION>
-										<OPTION value="40"> 40</OPTION>
-										<OPTION value="45"> 45</OPTION>
-										<OPTION value="50"> 50</OPTION>
-										<OPTION value="55"> 55</OPTION>
+										<OPTION value="?">min</OPTION>
+
+										<?php for ($i = 0; $i < 60; $i++) {
+											echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
+										} ?>
+
 									</SELECT>
 								</div>
 								<div class="col-2 padding text"> Dernier verre </div>
@@ -198,73 +143,48 @@
 									<!-- Select de l'heure du dernier verre -->
 									<SELECT name="dernierh">
 										<OPTION value="?"> h</OPTION>
-										<OPTION value="00"> 00h</OPTION>
-										<OPTION value="01"> 01h</OPTION>
-										<OPTION value="02"> 02h</OPTION>
-										<OPTION value="03"> 03h</OPTION>
-										<OPTION value="04"> 04h</OPTION>
-										<OPTION value="05"> 05h</OPTION>
-										<OPTION value="06"> 06h</OPTION>
-										<OPTION value="07"> 07h</OPTION>
-										<OPTION value="08"> 08h</OPTION>
-										<OPTION value="09"> 09h</OPTION>
-										<OPTION value="10"> 10h</OPTION>
-										<OPTION value="11"> 11h</OPTION>
-										<OPTION value="12"> 12h</OPTION>
-										<OPTION value="13"> 13h</OPTION>
-										<OPTION value="14"> 14h</OPTION>
-										<OPTION value="15"> 15h</OPTION>
-										<OPTION value="16"> 16h</OPTION>
-										<OPTION value="17"> 17h</OPTION>
-										<OPTION value="18"> 18h</OPTION>
-										<OPTION value="19"> 19h</OPTION>
-										<OPTION value="20"> 20h</OPTION>
-										<OPTION value="21"> 21h</OPTION>
-										<OPTION value="22"> 22h</OPTION>
-										<OPTION value="23"> 23h</OPTION>
+
+										<?php for ($i = 0; $i < 24; $i++) {
+											echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
+										} ?>
+
 									</SELECT>
 								</div>
 								<div class="col-1 padding marginlft">
 									<!-- Select des minutes du dernier verre -->
 									<SELECT name="derniermin">
-										<OPTION value="">min</OPTION>
-										<OPTION value="00"> 00</OPTION>
-										<OPTION value="05"> 05</OPTION>
-										<OPTION value="10"> 10</OPTION>
-										<OPTION value="15"> 15</OPTION>
-										<OPTION value="20"> 20</OPTION>
-										<OPTION value="25"> 25</OPTION>
-										<OPTION value="30"> 30</OPTION>
-										<OPTION value="35"> 35</OPTION>
-										<OPTION value="40"> 40</OPTION>
-										<OPTION value="45"> 45</OPTION>
-										<OPTION value="50"> 50</OPTION>
-										<OPTION value="55"> 55</OPTION>
+										<OPTION value="?">min</OPTION>
+
+										<?php for ($i = 0; $i < 60; $i++) {
+											echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
+										} ?>
+
 									</SELECT></div>
 							</div>
 						</div>
 						<div class="col-12 padding">
 							<div class="row">
 								<div class="offset-2"></div>
-								<div class="col-10 text">
-									Indiquez votre dosage, le nombre de verres consomm&eacute;s puis l'alcool comsomm&eacute;s
+								<div class="col-8 text">
+									Indiquez ci-dessous votre dosage d'alcool et le nombre de verres consomm&eacute;es
 								</div>
 							</div>
-							<div class="row padding">
-								<div class="offset-2"></div>
-								<div class="col-2">
-									<!-- Select des du dosage-->
-									<SELECT name="dosage">
-										<OPTION value="Dosage">Dosage</OPTION>
-										<OPTION value="bar"> BAR (STANDARD)</OPTION>
-										<OPTION value="maison"> MAISON </OPTION>
-										<OPTION value="fort"> FORT </OPTION>
-									</SELECT>
+							<div class="col-12 marginright">
+								<div class="row padding">
+									<div class="offset-4"></div>
+									<div class="col-2">
+										<!-- Select des du dosage-->
+										<SELECT name="dosage">
+											<OPTION value="Dosage">Dosage</OPTION>
+											<OPTION value="bar"> BAR (STANDARD)</OPTION>
+											<OPTION value="maison"> MAISON </OPTION>
+											<OPTION value="fort"> FORT </OPTION>
+										</SELECT>
+									</div>
+									<div class="col-2">
+										<input type="number" name="nbverres" required placeholder="0" />
+									</div>
 								</div>
-								<div class="col-3">
-								<input type="number" name="nbverres"/>
-								</div>
-								<div class="col-2"></div>
 							</div>
 						</div>
 						<div class="col-12 padding">
@@ -280,8 +200,6 @@
 							</div>
 						</div>
 						<div class="col-12 paddingfooter">
-
-
 						</div>
 				</div>
 				<div class="col-1">
@@ -289,28 +207,27 @@
 				</div>
 			</div>
 		</div>
+	</div>
+</body>
+<footer class="row">
+	<div class="col-12 footertext1">
+		Copyright © 2020 Amiens - France. Inc. Tous droits réservés.
+	</div>
+	<div class="offset-3"></div>
+	<div class="col-2 footertext2">CALCULATEUR</div>
+	<div class="col-2 footertext2">CONTRAT D'UTILISATION</div>
+	<div class="col-2 footertext2">MENTIONS L&Eacute;GALES</div>
+	<div class="offset-1"></div>
 
 	</div>
-	<footer class="row">
-		<div class="col-12 footertext1">
-			Copyright © 2020 Amiens - France. Inc. Tous droits réservés.
+	<div class="col-12 footerline"></div>
+	<div class="col-12">
+		<div class="row">
+			<div class="offset-4"></div>
+			<div class="col-2 footertext3">Developped and designed by :</div>
+			<div class="col-2 footertext4">Florian Wantelez, Mattei Freisi</div>
 		</div>
-		<div class="offset-3"></div>
-		<div class="col-2 footertext2">CALCULATEUR</div>
-		<div class="col-2 footertext2">CONTRAT D'UTILISATION</div>
-		<div class="col-2 footertext2">MENTIONS L&Eacute;GALES</div>
-		<div class="offset-1"></div>
-
-		</div>
-		<div class="col-12 footerline"></div>
-		<div class="col-12">
-			<div class="row">
-				<div class="offset-4"></div>
-				<div class="col-2 footertext3">Developped and designed by :</div>
-				<div class="col-2 footertext4">Florian Wantelez, Mattei Freisi</div>
-			</div>
-		</div>
-		</div>
-</body>
-
+	</div>
+	</div>
+</footer>
 </html>
