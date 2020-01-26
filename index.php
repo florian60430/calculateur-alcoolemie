4e3c4569a1f376a731d0e1ade59b944ba6894ad8
florian <fwantelez@la-providence.net>
2020-01-26 15:01:27 +0100

    Version lg fini

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="calculateur d'alcoolémie en ligne, vous permez de calculer et donner une aproximation de votre taux d'alcool dans le sang en g/l un conducteur possédant un permis probatoire ne peux pas conduire avec un taux supérieur à 20g/l alors qu'un conducteur confirmé ne peux pas conduire avec un taux supérieure à 0,50g/L" />
-	<meta name="keywords" content="alcoolémie, calculer son alcoolémie, taux d'alcoolémie, jeune conducteur, alcool, taux, taux d'alcool, alcool dans le sang" />
 
-	<link href="bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet"> <!-- Bootstrap (pour le design) -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> <!-- Bootstrap (pour le design) -->
 	<link href="style1.css" rel="stylesheet">
+	<link rel="stylesheet" media="screen and (max-width: 1200px)" href="ecran_lg.css" /> <!-- Pour ceux qui ont une résolution inférieure à 1200px -->
 	<link href="https://fonts.googleapis.com/css?family=Questrial&display=swap" rel="stylesheet"> <!-- Plice d'écriturte -->
 
 
@@ -26,8 +26,7 @@
 		</div>
 
 		<div class="row title2">
-			<div class="offset-4"></div>
-			<div class="col-4">
+			<div class="col-12">
 				D'ALCOOL&Eacute;MIE</div> <!-- 2ème ligne  Header -->
 		</div>
 		<div class="offset-4"></div>
@@ -36,20 +35,24 @@
 	<div class="container">
 		<div class="col-12">
 			<div class="row">
-				<div class="col-11 padding">
+				<div class="col-xl-11 col-lg-12 col-md-12 col-sm-12 padding">
 					<form method="post" action="traitement.php">
 						<div class="col-12 padding">
 							<div class="row">
-								<div class="offset-2"></div>
-								<div class="col-6 text">
+								<div class="offset-xl-2 offset-lg-2 offset-md-2 offset-sm-1"></div>
+								<div class="col-xl-6 col-lg-8 col-md-10 col-sm-12 d-none d-md-block text">
 									Indiquez votre sexe et votre poids dans les cases ci-dessous :
+								</div>
+
+								<div class="col-11 d-none d-sm-block d-md-none text">
+									Indiquez votre sexe et votre poids ci-dessous :
 								</div>
 							</div>
 						</div>
 						<div class="col-12 padding">
 							<div class="row">
 								<div class="offset-2"></div>
-								<div class="col-2">
+								<div class="col-xl-2 col-lg-2">
 									<!-- Select du sexe de l'utilisateur -->
 									<SELECT name="sexe" required>
 										<OPTION value=""> Sexe </OPTION>
@@ -59,11 +62,11 @@
 									</SELECT>
 								</div>
 								<div class="offset-1"></div>
-								<div class="col-2">
+								<div class="col-xl-2 col-lg-2">
 									<!-- formulaire du poids de l'utilisateur -->
 									<input required type="text" name="poid" value='' size="3" />
 								</div>
-								<div class="col-1 marginltf2 text">KG</div>
+								<div class="col-1 marginlft4 text">kg</div>
 							</div>
 						</div>
 						<div class="col-12 padding">
@@ -89,64 +92,76 @@
 									Indiquez l'heure à laquelle vous avez fini votre premier et dernier verre ci-dessous:
 								</div>
 								<div class="offset-2"></div>
-								<div class="col-2 padding text">Premier verre</div>
-								<div class="col-1 padding marginlft2">
-									<!-- Select de l'heure du premier verre -->
-									<SELECT name="premierh" required>
-										<OPTION value=""> h</OPTION>
+								<div class="col-xl-2 col-lg-2 padding text">Premier verre</div>
 
-										<?php for ($i = 0; $i < 24; $i++) {
-											echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
-										} ?>
 
-									</SELECT>
+								<div class="col-2 padding paddingheure">
+									<div class="row">
+										<div class="col-6 marginlft selectsize">
+											<!-- Select de l'heure du premier verre -->
+											<SELECT name="premierh" required>
+												<OPTION value=""> h</OPTION>
+
+												<?php for ($i = 0; $i < 24; $i++) {
+													echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
+												} ?>
+
+											</SELECT>
+										</div>
+										<div class="col-6 marginlft2">
+											<!-- Select des minutes du premier verre -->
+											<SELECT name="premiermin" required>
+												<OPTION value="">min</OPTION>
+
+												<?php for ($i = 0; $i < 60; $i++) {
+													echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
+												} ?>
+
+											</SELECT>
+										</div>
+									</div>
 								</div>
-								<!-- Select des minutes du premier verre -->
-								<div class="col-1 padding marginlft">
-									<SELECT name="premiermin" required>
-										<OPTION value="">min</OPTION>
+								<div class="col-xl-2 col-lg-2 padding text"> Dernier verre </div>
 
-										<?php for ($i = 0; $i < 60; $i++) {
-											echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
-										} ?>
+								<div class="col-2 padding paddingheure">
+									<div class="row">
+										<div class="col-6 marginlft">
+											<!-- Select de l'heure du dernier verre -->
+											<SELECT name="dernierh" required>
+												<OPTION value=""> h</OPTION>
 
-									</SELECT>
+												<?php for ($i = 0; $i < 24; $i++) {
+													echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
+												} ?>
+
+											</SELECT>
+										</div>
+										<div class="col-6 marginlft2">
+											<!-- Select des minutes du dernier verre -->
+											<SELECT name="derniermin" required>
+												<OPTION value="">min</OPTION>
+
+												<?php for ($i = 0; $i < 60; $i++) {
+													echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
+												} ?>
+
+											</SELECT>
+										</div>
+									</div>
 								</div>
-								<div class="col-2 padding text"> Dernier verre </div>
-								<div class="col-1 padding marginlft2">
-									<!-- Select de l'heure du dernier verre -->
-									<SELECT name="dernierh" required>
-										<OPTION value=""> h</OPTION>
-
-										<?php for ($i = 0; $i < 24; $i++) {
-											echo "<OPTION value=" . $i . "> " . $i . "h</OPTION>";
-										} ?>
-
-									</SELECT>
-								</div>
-								<div class="col-1 padding marginlft">
-									<!-- Select des minutes du dernier verre -->
-									<SELECT name="derniermin" required>
-										<OPTION value="">min</OPTION>
-
-										<?php for ($i = 0; $i < 60; $i++) {
-											echo "<OPTION value=" . $i . "> " . $i . "min</OPTION>";
-										} ?>
-
-									</SELECT></div>
 							</div>
 						</div>
 						<div class="col-12 padding">
 							<div class="row">
 								<div class="offset-2"></div>
-								<div class="col-8 text">
+								<div class="col-xl-8 col-lg-10 text">
 									Indiquez ci-dessous votre dosage d'alcool et le nombre de verres consomm&eacute;es
 								</div>
 							</div>
-							<div class="col-12 marginright">
+							<div class="col-12 ">
 								<div class="row padding">
-									<div class="offset-4"></div>
-									<div class="col-2">
+									<div class="offset-xl-4 offset-lg-4"></div>
+									<div class="col-xl-2 col-lg-2 marginlft3">
 										<!-- Select des du dosage-->
 										<SELECT name="dosage" required>
 											<OPTION value="">Dosage</OPTION>
@@ -155,7 +170,7 @@
 											<OPTION value="fort"> FORT </OPTION>
 										</SELECT>
 									</div>
-									<div class="col-2">
+									<div class="col-xl-2 col-lg-2 marginright">
 										<input type="number" name="nbverres" required placeholder="0" />
 									</div>
 								</div>
@@ -163,7 +178,7 @@
 						</div>
 						<div class="col-12 padding">
 							<div class="row">
-								<div class="offset-5"></div>
+								<div class="offset-xl-5 offset-lg-6"></div>
 								<div class="col-3 padding">
 									<form method="post" action="traitement.php">
 										<button type="submit" class="size">
@@ -176,7 +191,7 @@
 						<div class="col-12 paddingfooter">
 						</div>
 				</div>
-				<div class="col-1">
+				<div class="col-1 d-none d-xl-block">
 					<img src="img/picture2.jpg" alt="img cool">
 				</div>
 			</div>
